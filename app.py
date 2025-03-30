50183c1132f3a5914b336a97c5a438d44f6b0746
Kaysaami <kaysaami@gmail.com>
2025-03-30 09:15:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,5 @@ def create_app(config_name='development'):
     return app
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 8080))  # Use DigitalOcean's PORT, default to 8080
-    app.run(host="0.0.0.0", port=port)
+    app = create_app('development')
+    app.run(debug=True)
